125dacd8150f8c92b1c9c1014a42f7799dd99c27
Le Thu Nguyen <nglthu1967@gmail.com>
2025-05-31 19:49:23 +0700

    Delete mydemo directory

--- mydemo/src/App.java
+++
@@ -1,7 +0,0 @@
-import Test.testUserBook;
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        testUserBook.Display();
-    }
-}
