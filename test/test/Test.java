e60d94b09b7e09c343fbefb61c79b8dbb1469d45
nglthu1979 <nglthu1967@gmail.com>
2025-06-03 22:51:10 +0700

    test update

--- test/test/Test.java
+++ test/test/Test.java
@@ -1,22 +1,76 @@
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-
+import java.time.LocalDate;
 public class Test {
     public static void test() {
 
+
+        //book
+
+        //Database
+        Book book = new Book("D000001", "Database Design for Mere Mortals: 25th Anniversary Edition", " Michael J Hernandez ", "Addison-Wesley Professional", "0136788041");
+        Book book2 = new Book("D000002", "Fundamentals of Data Engineering: Plan and Build Robust Data Systems", " Joe Reis", "O'Reilly Media", "1098108302");
+
+        //Mobile Application
+        Book book3 = new Book("M000001", "Agile Project Management for Mobile Application Development", "John Estrella ", "Agilitek Corporation", "978-1544922478");
+        Book book4 = new Book("M000002", "Building Mobile Apps at Scale: 39 Engineering Challenges", "Gergely Orosz ", "Primedia E-launch LLC", "1638778868");
+
+        //Web
+        Book book5 = new Book("W000001", "Full Stack Web Development: A Comprehensive, Hands-On Guide to Building Modern Websites and Applications", "Philip Ackermann", "Thanh Nien", "1493224379");
+        Book book6 = new Book("W000002", "Web Technology", "Shraddha N. Zanjat", "Lambert", "620747029X");
+        //Java
+        Book book7 = new Book("J000001", "Thinking in Java", "Bruce Eckel", "Rheinwerk Computing", "0131872486");
+        Book book8 = new Book("J000002", "Head First Java: A Brain-Friendly Guide Edition", "Kathy Sierra","O'Reilly", "ISBN98349098");
+
+        //add to library
+        Library lib1 = new Library(book);
+        lib1.setBooks(book2);
+        lib1.setBooks(book3);
+        lib1.setBooks(book6);
+        lib1.setBooks(book4);
+        lib1.setBooks(book5);
+        lib1.setBooks(book6);
+        lib1.setBooks(book7);
+        lib1.setBooks(book8);
+
+        //Tim tat ca sach theo id or author
+        System.out.println("Begin search book by author and publisher");
+        String author = UserInput.inputString("Author");
+        String publisher = UserInput.inputString("Publisher");
+
+        //check here 2 conditions to add book
+        lib1.getBooks(author, publisher);
+        Library.printBooks(lib1.getBooks(author, publisher));
+
+        //thong ke: So luong sach theo tac gia vua nhap
+        System.out.println(" Author : " + author + " co  " +  Library.staticBooks(lib1.getBooks(author, publisher), author)  + " books");
+
+        System.out.println("end search book by author and publisher");
+        //
+
+
+
         // user List
-        User u1 = new User("gfz", "Nguyen Thi Van");
-        User u2 = new User("123333", "tran van Nguyen");
+
+        User u1 = new User("u00001", "Nguyen Thi Van");
+        User u2 = new User("u00002", "Tran van Nguyen");
+        User u3 = new User("u00003", "Hoang Thi Lan");
+        User u4 = new User("u00004", "Nguyen Thi Van Lan");
 
         ListUser listuser = new ListUser();
         listuser.addUser(u2);
         listuser.addUser(u1);
 
-        System.out.println("test");
-        Userbook ub0 = new Userbook("123", "abc");
-        Userbook ub1 = new Userbook("1233", "4563");
-        Userbook ub2 = new Userbook("gfz", "456333");
-        Userbook ub3 = new Userbook("123333", "gfz");
+        System.out.println("Books borrowed by Users");
+        LocalDate currentDate = LocalDate.now();
+
+        Userbook ub0 = new Userbook("u00001", "M000001", currentDate);
+        Userbook ub1 = new Userbook("u00002", "M000001", currentDate);
+        Userbook ub2 = new Userbook("u00001", "J000001", currentDate);
+        Userbook ub3 = new Userbook("u00002", "D000001", currentDate);
+        Userbook ub4 = new Userbook("u00003", "D000001", currentDate);
+        Userbook ub5 = new Userbook("u00004", "D000001", currentDate);
 
         ListUserbook ub = new ListUserbook();
 
@@ -24,36 +78,41 @@ public class Test {
         ub.addObject(ub1);
         ub.addObject(ub2);
         ub.addObject(ub3);
+        ub.addObject(ub4);
+        ub.addObject(ub5);
 
-        System.out.println("test short");
 
-        ub.shortList();
 
-        System.out.println("test filter:");
-        ub.displayList(ub.filterUserBook("gfz"));
+        System.out.println("Test filter book by any keyword matching to user id or userbook:");
+
+        String keyword = UserInput.inputString("keyword");
+        System.out.println("keyword : " + keyword);
+
+        //ub.displayList(ub.filterUserBook(keyword));
 
         // Userbook filter
         //List sau khi search
         //search in Lamda expression
-        //List<Userbook> listUserbook = ub.filterUserBook("gfz");
+        //keyword userid or book id
+       // List<Userbook> listUserbook = ub.filterUserBook(keyword);
+       // System.out.println("listUserbook size: " + listUserbook.size());
 
-        // search in traidtional expression
-        List<Userbook> listUserbook = ub.filterUserBookTradition("gfz");
+        // search in traditional expression
 
-
+       List<Userbook> listUserbook = ub.filterUserBookTradition(keyword);
 
         // Hien thi name theo Lamda code
-        //todo: ListUserbook: already filtered list with key word
-        /*
-        for (int i =0 ; i<listUserbook.size(); i++)
+        // todo: ListUserbook: already filtered list with key word
+
+        for (int i =0 ; i <listUserbook.size(); i++)
         {
          listuser.searchUserName(listUserbook.get(i).userid);
           listuser.displayList(listuser.searchUserName(listUserbook.get(i).userid));
         }
       
-        */
-       //hien thi name theo cach viet truye thong
 
+       //hien thi name theo cach viet truye thong
+/*
         for (int i =0 ; i<listUserbook.size(); i++)
         {
             listuser.searchUserName(listUserbook.get(i).userid);
@@ -61,7 +120,7 @@ public class Test {
             listuser.displayList(listuser.searchUserNameTradition(listUserbook.get(i).userid));
         }
 
-
+*/
 
     }
 }
